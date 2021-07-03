cb2484ae432ebfafcc4016bc1ba8c06f753b5a9a
nhat05 <nhat05thud@gmail.com>
2021-07-04 04:01:16 +0700

    UPDATE: integrate media picker to modal BlogCategory

---
+++ src/Tankerz.Web/Pages/BlogCategories/CreateModal.cshtml.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Tankerz.BlogCategories;
+using Volo.Abp.AspNetCore.Mvc.UI.Bootstrap.TagHelpers.Form;
+
+namespace Tankerz.Web.Pages.BlogCategories
+{
+    public class CreateModalModel : TankerzPageModel
+    {
+        [BindProperty]
+        public CreateBlogCategoryViewModel BlogCategory { get; set; }
+
+        private readonly IBlogCategoryAppService _blogCategoryAppService;
+
+        public CreateModalModel(IBlogCategoryAppService blogCategoryAppService)
+        {
+            _blogCategoryAppService = blogCategoryAppService;
+        }
+
+        public void OnGet()
+        {
+            BlogCategory = new CreateBlogCategoryViewModel();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var dto = ObjectMapper.Map<CreateBlogCategoryViewModel, CreateUpdateBlogCategoryDto>(BlogCategory);
+            await _blogCategoryAppService.CreateAsync(dto);
+            return NoContent();
+        }
+
+        public class CreateBlogCategoryViewModel
+        {
+            public string Banners { get; set; }
+            public string Images { get; set; }
+            [Required]
+            [StringLength(256)]
+            public string Name { get; set; }
+            [TextArea]
+            public string Description { get; set; }
+            public string Tags { get; set; }
+            public int Priority { get; set; }
+            public bool IsPublish { get; set; }
+            public bool IsShowOnMenu { get; set; }
+            public bool IsShowOnHomePage { get; set; }
+
+
+            public string MetaTitle { get; set; }
+            public string MetaDescription { get; set; }
+            public string MetaKeyword { get; set; }
+            public string MetaTag { get; set; }
+            public string MetaThumbnail { get; set; }
+        }
+    }
+}
--- theme/Abp.AspNetCore.Mvc.UI.Theme.Front/wwwroot/assets/js/custom-script.js
+++ theme/Abp.AspNetCore.Mvc.UI.Theme.Front/wwwroot/assets/js/custom-script.js
@@ -1,4 +1,17 @@
-﻿var sidebar = $('.js-navbar-vertical-aside').hsSideNav();
+﻿$.fn.modal.Constructor.prototype._enforceFocus = function () {
+    modal_this = this
+    $(document).on('focusin', function (e) {
+        if (modal_this.$element != null) {
+            if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length
+                && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select')
+                && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
+                modal_this.$element.focus()
+            }
+        }
+    })
+};
+
+var sidebar = $('.js-navbar-vertical-aside').hsSideNav();
 $('.js-nav-tooltip-link').tooltip({ boundary: 'window' })
 $(".js-nav-tooltip-link").on("show.bs.tooltip", function (e) {
     if (!$("body").hasClass("navbar-vertical-aside-mini-mode")) {
